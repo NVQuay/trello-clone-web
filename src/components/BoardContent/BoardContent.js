69262ff0dedbd2756c622ac2cdde83917125d064
NVQuay <tsebyuaq20111998@gmail.com>
2022-08-15 15:48:03 +0700

    implement drag drop

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -4,6 +4,7 @@ import "./BoardContent.scss";
 import { initialData } from "actions/initialData";
 import { isEmpty } from "lodash";
 import { mapOrder } from "utilities/sorts";
+import { Container, Draggable } from "react-smooth-dnd";
 
 function BoardContent() {
   const [board, setBoard] = useState({});
@@ -22,16 +23,33 @@ function BoardContent() {
 
   if (isEmpty(board)) {
     return (
-      <div className="not-found" style={{ padding: "10px", color: "white" }}>
+      <div className='not-found' style={{ padding: "10px", color: "white" }}>
         Board not found
       </div>
     );
   }
+  const onColumnDrop = (dropResult) => {
+    console.log(dropResult);
+  };
   return (
-    <div className="board-content">
-      {columns.map((column, index) => (
-        <Column key={index} column={column} />
-      ))}
+    <div className='board-content'>
+      <Container
+        orientation='horizontal'
+        onDrop={onColumnDrop}
+        getChildPayload={(index) => columns[index]}
+        dragHandleSelector='.column-drag-handle'
+        dropPlaceholder={{
+          animationDuration: 150,
+          showOnTop: true,
+          className: "column-drop-preview",
+        }}
+      >
+        {columns.map((column, index) => (
+          <Draggable key={index}>
+            <Column column={column} />
+          </Draggable>
+        ))}
+      </Container>
     </div>
   );
 }
