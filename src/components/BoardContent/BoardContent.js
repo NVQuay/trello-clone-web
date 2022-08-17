9742c5602849c6a08fa06ceac1d4a096b8eaec3f
NVQuay <tsebyuaq20111998@gmail.com>
2022-08-17 11:44:19 +0700

    Implement boostrap, react-boostrap, add new column

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import Column from "components/Column/Column";
 import "./BoardContent.scss";
 import { initialData } from "actions/initialData";
@@ -6,10 +6,25 @@ import { isEmpty } from "lodash";
 import { mapOrder } from "utilities/sorts";
 import { applyDrag } from "utilities/dragDrop";
 import { Container, Draggable } from "react-smooth-dnd";
+import {
+  Container as BootstrapContainer,
+  Row,
+  Col,
+  Form,
+  Button,
+} from "react-bootstrap";
 
 function BoardContent() {
   const [board, setBoard] = useState({});
   const [columns, setColumns] = useState([]);
+  const [openNewColumnForm, setOpenNewColumnForm] = useState(false);
+
+  const newColumnInputRef = useRef(null);
+
+  const [newColumnTitle, setNewColumnTitle] = useState("");
+  const onNewColumnTitleChange = useCallback((e) => {
+    setNewColumnTitle(e.target.value);
+  }, []);
 
   useEffect(() => {
     const boardFromDB = initialData.boards.find(
@@ -21,6 +36,13 @@ function BoardContent() {
       setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, "id"));
     }
   }, []);
+
+  useEffect(() => {
+    if (newColumnInputRef && newColumnInputRef.current) {
+      newColumnInputRef.current.focus();
+      newColumnInputRef.current.select();
+    }
+  }, [openNewColumnForm]);
 
   if (isEmpty(board)) {
     return (
@@ -51,7 +73,34 @@ function BoardContent() {
       setColumns(newColumns);
     }
   };
+  const ToggleOpenNewColumnForm = () =>
+    setOpenNewColumnForm(!openNewColumnForm);
 
+  const addNewColumn = () => {
+    if (!newColumnTitle) {
+      newColumnInputRef.current.focus();
+      return;
+    }
+    const newColumnToAdd = {
+      id: Math.random().toString(36).substring(2, 5), // 5 random characters, will remove when we implement code api
+      boardId: board.id,
+      title: newColumnTitle.trim(),
+      cardOrder: [],
+      cards: [],
+    };
+
+    let newColumns = [...columns];
+    newColumns.push(newColumnToAdd);
+
+    let newBoard = { ...board };
+    newBoard.columnOrder = newColumns.map((c) => c.id);
+    newBoard.columns = newColumns;
+
+    setColumns(newColumns);
+    setBoard(newBoard);
+    setNewColumnTitle("");
+    ToggleOpenNewColumnForm();
+  };
   return (
     <div className='board-content'>
       <Container
@@ -71,9 +120,41 @@ function BoardContent() {
           </Draggable>
         ))}
       </Container>
-      <div className='add-new-column'>
-        <i className='icon fa fa-plus'></i>Add another column
-      </div>
+
+      <BootstrapContainer className='tsebyuaqdev-trelo-container'>
+        {!openNewColumnForm && (
+          <Row>
+            <Col className='add-new-column' onClick={ToggleOpenNewColumnForm}>
+              <i className='icon fa fa-plus'></i>Add another column
+            </Col>
+          </Row>
+        )}
+        {openNewColumnForm && (
+          <Row>
+            <Col className='enter-new-column'>
+              <Form.Control
+                size='sm'
+                type='text'
+                placeholder='Enter column title...'
+                className='input-enter-new-column'
+                ref={newColumnInputRef}
+                value={newColumnTitle}
+                onChange={onNewColumnTitleChange}
+                onKeyDown={(event) => event.key === "Enter" && addNewColumn()}
+              />
+              <Button variant='success' size='sm' onClick={addNewColumn}>
+                Add column
+              </Button>
+              <span
+                className='cancel-new-column'
+                onClick={ToggleOpenNewColumnForm}
+              >
+                <i className='fa fa-trash icon'></i>
+              </span>
+            </Col>
+          </Row>
+        )}
+      </BootstrapContainer>
     </div>
   );
 }
